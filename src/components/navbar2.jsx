b07bc0bdfd94185a5791d5d2bc55bed60c5fafc4
BvakachaTakunda <takundabvax@gmail.com>
2024-09-02 07:13:05 -0700

    few

--- src/components/navbar2.jsx
+++ src/components/navbar2.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-links" style={{display:"flex", margin:"0px"}}>
-        <Link to="/Host" className="active">Dashboard</Link>
+        <Link to="/Host" className="active"><h6 style={{ margin:"0"}}>Dashboard</h6></Link>
         <Link to="/Income"><h6 style={{ margin:"1px"}}>Income</h6></Link>
         <Link to="/Vansdash"><h6 style={{ margin:"1px"}}>Vans</h6></Link>
         <Link to="/Reviews"><h6 style={{ margin:"1px"}}>Reviews</h6></Link>
