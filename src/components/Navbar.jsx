94e7be8f4c83fd1d6ce7f78af9cdfceec5d9feb1
BvakachaTakunda <takundabvax@gmail.com>
2024-09-16 13:13:35 -0700

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,11 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo" ><Link to="/HeroSection"><h4 style={{margin:"0px", color:"black"}}>#CarRental</h4></Link></div>
+      <div className="logo" ><Link to="/HeroSection"><h4 style={{margin:"0px", color:"black", marginLeft:"40px"}}>#CarRental</h4></Link></div>
       <div className="nav-links" style={{display:"flex", margin:"0px"}}>
         <Link to="/Rent/:name"><h4 style={{ margin:"1px"}}>Host</h4></Link>
         <Link to="/About"><h4 style={{ margin:"1px"}}>About</h4></Link>
-        <Link to="/Vans"><h4 style={{ margin:"1px"}}>Vans</h4></Link>
+        <Link to="/Vans"><h4 style={{ margin:"1px",marginRight:"40px"}}>Vans</h4></Link>
       </div>
     </nav>
   );
